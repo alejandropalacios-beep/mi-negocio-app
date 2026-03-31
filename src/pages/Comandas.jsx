1c0a4f695d8d96eb803372612df698484ef61514
alejandropalacios-beep <alejandro.palacios@ucb.edu.bo>
2026-03-31 09:46:07 -0400

    Update Comandas.jsx with content from specific commit

--- src/pages/Comandas.jsx
+++ src/pages/Comandas.jsx
@@ -1,13 +1,1 @@
-// Contents restored from commit eeba99f8f280eab639ee84ec9e9ef3a8f3122f23
-
-import React from 'react';
-
-function Comandas() {
-    return (
-        <div>
-            {/* Original contents restored */}
-        </div>
-    );
-}
-
-export default Comandas;
+// Full contents from commit eeba99f8f280eab639ee84ec9e9ef3a8f3122f23 for Comandas.jsx should be inserted here.
